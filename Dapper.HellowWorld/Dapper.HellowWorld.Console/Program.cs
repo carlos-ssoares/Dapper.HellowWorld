d6b9f17578cddd7cd6ba0d29a41d53101fdbfe39
carlos-ssoares <carlos.soares@live.com>
2019-10-27 21:33:19 -0400

    Creating project

---
+++ Dapper.HellowWorld/Dapper.HellowWorld.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.HellowWorld.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void getObjects()
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString))
+            {
+                IDbCommand cmd = db.CreateCommand();
+                cmd.CommandText = "Select * From Author";
+
+                
+            }
+        }
+    }
+}
